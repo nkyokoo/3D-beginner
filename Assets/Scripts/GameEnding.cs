a60d0e80ae1c78c1c7c035f3d4529c3894805d3d
kyoko <notorious.droid@gmail.com>
2020-06-19 14:27:58 +0200

    finished guide

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
     public float displayImageDuration = 1f;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
+
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
-    
+    bool m_HasAudioPlayed;
+
 
     void OnTriggerEnter (Collider other)
     {
@@ -22,23 +29,48 @@ public class GameEnding : MonoBehaviour
         }
 
     }
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
     void Update()
     {
         if(m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
 
+        }  
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
         
         
     }
-    void EndLevel ()
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource) 
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
         if(m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                Debug.Log(doRestart);
+                SceneManager.LoadScene(0);
+
+            }
+            else
+            {
+                Application.Quit();
+
+            }
+
+        }
+        if(!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+
 
         }
     }
